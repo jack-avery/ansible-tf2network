faa842e6422595e7f5412c7765c4873780c023fa
jack-avery <email.jack.avery@gmail.com>
2024-06-14 05:49:21 -0400

    have manifest.py retain original file contents

--- manifest.py
+++ manifest.py
@@ -16,16 +16,20 @@ import time
 import yaml
 
 
-def decrypt_ansible_secrets() -> None:
-    logging.info("Decrypting secrets...")
-    os.system("ansible-vault decrypt host_vars/*.secret.yml")
-    os.system("ansible-vault decrypt inventory.yml")
-
-
 def load_ansible_inventory() -> dict:
     logging.info("Loading Ansible inventory...")
+
+    # cache original and decrypt
+    with open("inventory.yml") as inventory_yml:
+        original = inventory_yml.read()
+    os.system("ansible-vault decrypt inventory.yml")
+
     with open("inventory.yml") as inventory_yml:
         inventory = yaml.safe_load(inventory_yml)
+    
+    # re-write original
+    with open("inventory.yml", "w") as inventory_yml:
+        inventory_yml.write(original)
 
     retval = inventory["all"]["children"]["tf2"]["children"]["prod"]["hosts"]
     logging.debug(retval)
@@ -45,38 +49,57 @@ def load_ansible_globals() -> dict:
 
 
 def load_ansible_variables() -> dict:
+    logging.info("Discovering secrets...")
+    secrets: list[str] = []
+    secrets += [
+        file
+        for file in os.listdir("host_vars")
+        if not file.endswith(".sample")
+    ]
+    logging.debug(secrets)
+
     logging.info("Loading Ansible host variables...")
     host_vars = {}
-    for file in os.listdir("host_vars"):
-        if file.endswith(".sample"):
-            continue
+    for file in secrets:
+        is_secret: bool = False
+        file_path: str = f"host_vars/{file}"
 
-        is_secret = False
         if file.endswith(".secret.yml"):
             is_secret = True
+
+            # cache original and decrypt
+            with open(file_path) as secrets_file:
+                original = secrets_file.read()
+            os.system(f"ansible-vault decrypt {file_path}")
+
             host_strip = 11  # len(".secret.yml")
         else:
             host_strip = 4  # len(".yml")
         host = file[:-host_strip]
 
-        with open(f"host_vars/{file}") as host_yml:
+        with open(file_path) as host_yml:
             vars = yaml.safe_load(host_yml)
 
-            if host not in host_vars:
-                host_vars[host] = {}
+        if host not in host_vars:
+            host_vars[host] = {}
 
-            if is_secret:
-                instances_secrets = {
-                    instance["name"]: instance for instance in vars["instances_secrets"]
-                }
-                host_vars[host]["secret"] = instances_secrets
-            else:
-                instances = {
-                    instance["name"]: instance for instance in vars["instances"]
-                }
-                del vars["instances"]
-                host_vars[host]["host"] = vars
-                host_vars[host]["vars"] = instances
+        if is_secret:
+            instances_secrets = {
+                instance["name"]: instance for instance in vars["instances_secrets"]
+            }
+            host_vars[host]["secret"] = instances_secrets
+
+            # re-write original
+            with open(file_path, "w") as secrets_file:
+                secrets_file.write(original)
+
+        else:
+            instances = {
+                instance["name"]: instance for instance in vars["instances"]
+            }
+            del vars["instances"]
+            host_vars[host]["host"] = vars
+            host_vars[host]["vars"] = instances
 
     logging.debug(host_vars)
     return host_vars
@@ -127,7 +150,6 @@ def create_manifest(inventory: dict, globals: dict, vars: dict) -> dict:
 
 
 def main() -> None:
-    decrypt_ansible_secrets()
     ansible_inv = load_ansible_inventory()
     ansible_globals = load_ansible_globals()
     ansible_vars = load_ansible_variables()
