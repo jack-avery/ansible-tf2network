33c31eabdf985c8e80d2a82c0583a4774eea3e91
jack-avery <47289484+jack-avery@users.noreply.github.com>
2024-07-18 20:16:13 -0400

    summer update

--- roles/base-tf2server/files/update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd $STEAMCMDDIR
-
-mkdir $HOMEDIR/.lock
-touch $HOMEDIR/.lock/lock
-
-./steamcmd.sh +force_install_dir $HOMEDIR/tf-dedicated +login anonymous +app_update 232250 +quit
-
-rm -r $HOMEDIR/.lock
