70c44b4267b56a0a5243ee1bb767b2a6e4f3fe6e
jack-avery <47289484+jack-avery@users.noreply.github.com>
2025-08-04 21:09:30 -0400

    fix(updater): make robust, remove edge cases

--- roles/deploy/files/updater.sh
+++ roles/deploy/files/updater.sh
@@ -5,88 +5,97 @@
 set -eu
 
 get_networks() {
-    NETWORKS=()
-    for network in /home/tf2server/build/servers/*; do
-        network=${network##*/}
-        NETWORKS+=( $network )
-    done
+	NETWORKS=()
+	for network in /home/tf2server/build/servers/*; do
+		network=${network##*/}
+		NETWORKS+=( $network )
+	done
 }
 
 get_servers_for_network() {
-    SERVERS=()
-    for server in /home/tf2server/build/servers/$1/*; do
-        server=${server##*/}
-        SERVERS+=( $server )
-    done
+	SERVERS=()
+	for server in /home/tf2server/build/servers/$1/*; do
+		server=${server##*/}
+		SERVERS+=( $server )
+	done
+}
+
+log() {
+	echo "[$(date +'%D %H:%M:%S')] ($$) $1"
+}
+
+die() {
+	if [ -e updater-active.lock ]; then
+		rm updater-active.lock
+	fi
+	log "Finished. Exit message: $1"
+	exit 0
 }
 
 main() {
-    if [ -f updater-active.lock ]; then
-        echo "[$(date +'%D %H:%M:%S')] ($$) A job is already active."
-        exit 0
-    fi
-    touch updater-active.lock
+	if [ -f updater-active.lock ]; then
+		log "A job is already active."
+		exit 0
+	fi
+	touch updater-active.lock
 
-    echo ""
-    echo "[$(date +'%D %H:%M:%S')] ($$) Checking..."
-    UPDATE_TIMEOUT=30
-    LATEST=$(docker run --quiet --pull always --rm --entrypoint=/bin/bash cm2network/steamcmd -c "timeout $UPDATE_TIMEOUT ./steamcmd.sh +login anonymous +app_info_print 232250 +login anonymous +app_info_print 232250 +logout +quit" | perl -n -e'/"buildid"\s+"([    0-9]+)"/ && print "$1\n"' | head -n1)
+	echo ""
+	log "Checking..."
+	UPDATE_TIMEOUT=30
+	LATEST=$(docker run --quiet --pull always --rm --entrypoint=/bin/bash cm2network/steamcmd -c "timeout $UPDATE_TIMEOUT ./steamcmd.sh +login anonymous +app_info_print 232250 +login anonymous +app_info_print 232250 +logout +quit" | perl -n -e'/"buildid"\s+"([    0-9]+)"/ && print "$1\n"' | head -n1)
 
-    if [ ! -f current_tf2_buildid ]; then
-        echo "[$(date +'%D %H:%M:%S')] No current version saved, saving latest as current and doing nothing!"
-        echo $LATEST > current_tf2_buildid
+	if [ ! -f current_tf2_buildid ]; then
+		echo $LATEST > current_tf2_buildid
+		die "No current version saved, saving and exiting."
+	fi
+	CURRENT=$(cat current_tf2_buildid)
 
-        rm updater-active.lock
-        exit 0
-    fi
-    CURRENT=$(cat current_tf2_buildid)
+	if [ -z "$LATEST" ]; then
+		die "Failed: could not fetch latest"
 
-    echo "[$(date +'%D %H:%M:%S')] Current: $CURRENT; Latest: $LATEST"
+	elif [[ "$CURRENT" -ne "$LATEST" ]]; then
+		log "Current: $CURRENT; Latest: $LATEST"
 
-    if [ -z "$LATEST" ]; then
-        echo "[$(date +'%D %H:%M:%S')] Failed to fetch latest, doing nothing"
+		# rebuild (update) the base image
+		log "Reached base rebuild phase."
+		if ! docker buildx build --no-cache -t jackavery/base-tf2server:latest /home/tf2server/build/base; then
+			die "Failed: base image failed to build"
+		fi
 
-        rm updater-active.lock
-        exit 0
+		# rebuild servers
+		log "Reached network rebuild phase."
+		get_networks
+		for network in ${NETWORKS[@]} ; do
+			log "Processing $network..."
 
-    elif [[ "$CURRENT" -ne "$LATEST" ]]; then
-        # rebuild (update) the base image
-        echo "[$(date +'%D %H:%M:%S')] Updating base image..."
-        docker buildx build --no-cache -t jackavery/base-tf2server:latest /home/tf2server/build/base
+			get_servers_for_network $network
 
-        get_networks
+			for server in ${SERVERS[@]} ; do
+				log "Rebuilding $network/$server..."
+				cd /home/tf2server/build/servers/$network/$server
+				if ! docker buildx build -t srcds-$network-$server:latest .; then
+					log "Warning: failed to build image for Network: $network Server: $server"
+				fi
+				cd /home/tf2server
+			done
 
-        # rebuild servers
-        for network in ${NETWORKS[@]} ; do
-            echo "[$(date +'%D %H:%M:%S')] Processing $network..."
+			log "Docker recomposing $network..."
+			if ! docker compose -f /home/tf2server/docker-compose_$network.yml up -d; then
+				log "Warning: failed to compose $network"
+			fi
+		done
 
-            get_servers_for_network $network
+		log "Reached cleanup phase."
+		log "Pruning Docker..."
+		docker system prune -f
 
-            for server in ${SERVERS[@]} ; do
-                echo "[$(date +'%D %H:%M:%S')] Rebuilding $network/$server..."
-                cd /home/tf2server/build/servers/$network/$server
-                docker buildx build -t srcds-$network-$server:latest .
-                cd /home/tf2server
-            done
+		log "Saving latest version $LATEST..."
+		echo $LATEST > current_tf2_buildid
 
-            echo "[$(date +'%D %H:%M:%S')] Docker recomposing $network..."
-            docker compose -f /home/tf2server/docker-compose_$network.yml up -d
-        done
-
-        echo "[$(date +'%D %H:%M:%S')] Pruning Docker..."
-        docker system prune -f
-
-        echo $LATEST > current_tf2_buildid
-
-        rm updater-active.lock
-        exit 0
-
-    else
-        echo "[$(date +'%D %H:%M:%S')] Current is latest, nothing to do"
-
-        rm updater-active.lock
-        exit 0
-    fi
+		die "Update was successful!"
+	else
+		die "Current is latest, nothing to do"
+	fi
 }
 
 main $@
